f97ef29db05ca9e9bb812fb296c1b8b2a003ccb2
Paula Andrea Anaya Ramirez <panaya@unbosque.edu.co>
2021-03-11 21:36:46 -0500

    Adding table and removing cards

--- js/app.js
+++ js/app.js
@@ -1,6 +1,39 @@
 const addAnimal = document.getElementById("btn-addAnimal");
-const formAddAnimal = document.querySelector("#container-form");
+const formAddAnimal = document.querySelector("#cr-form");
 const btnCloseForm = document.querySelector(".btn-close");
+
+
+// Table functions 
+$('#DataPets').DataTable({
+    order: [
+        [3, 'desc']
+    ],
+    pagingType: 'full_numbers',
+    lengthMenu: [
+        [5, 10, 25, 50, -1],
+        [5, 10, 25, 50, "All"]
+    ],
+    initComplete: function() {
+        this.api().columns().every(function() {
+            var column = this;
+            var select = $('<select><option value=""></option></select>')
+                .appendTo($(column.footer()).empty())
+                .on('change', function() {
+                    var val = $.fn.dataTable.util.escapeRegex(
+                        $(this).val()
+                    );
+
+                    column
+                        .search(val ? '^' + val + '$' : '', true, false)
+                        .draw();
+                });
+
+            column.data().unique().sort().each(function(d, j) {
+                select.append('<option value="' + d + '">' + d + '</option>')
+            });
+        });
+    }
+});
 
 
 addAnimal.addEventListener("click", () => {
