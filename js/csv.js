ba210833acb906d75dfbeef78407f69bf49ba11a
cdsanchezm <64279165+cdsanchezm@users.noreply.github.com>
2021-03-18 19:49:12 -0500

    add pet information in table

--- js/csv.js
+++ js/csv.js
@@ -31,3 +31,66 @@ d3.dsv(";", "./data/pets-citizens.csv", function(d) {
         console.log('error', error);
 
     });
+
+    // var buttonSave;
+    // var namePet;
+    // var namePerson;
+    // var race;
+    // var direccion;
+    // var latitude;
+    // var longitude;
+    // var species;
+    // var neighborhood;
+    // var size;
+    // var image;
+    // namePet = document.querySelector("#NamePet").value;
+    // namePerson = document.querySelector("#NamePerson").value;
+    // race = document.querySelector("#race").value;
+    // direccion = document.querySelector("#direccion").value
+    // latitude = document.querySelector("#latitudFA").value;
+    // longitude = document.querySelector("#LongitudFA").value;
+    // species = document.querySelector("#species").value;
+    // neighborhood = document.querySelector("#neighborhood").value;
+    // size = document.querySelector("#size").value;
+    // image = document.querySelector("#image").value
+
+    // console.log(namePet, namePerson, race, direccion, latitude, longitude, species, neighborhood,size,image);
+  var form = document.getElementById("formAdd");
+     
+    buttonSave = document.getElementById("buttonsave");
+    buttonSave.addEventListener("click", () =>  {
+
+         
+        var dataf = new FormData(form);
+        insertRowinTable(dataf)
+        console.log("hola");
+    
+})
+
+function insertRowinTable(dataf ){
+    var TableRef = document.getElementById("example");
+    var TableRowRef = TableRef.insertRow(1);
+
+    var NewCellRef = TableRowRef.insertCell(0);
+    NewCellRef.textContent = document.querySelector("#NamePerson").value;
+    console.log(dataf.get("NamePerson"));
+
+     NewCellRef = TableRowRef.insertCell(1);
+     NewCellRef.textContent = dataf.get("species");
+
+     NewCellRef = TableRowRef.insertCell(2);
+     NewCellRef.textContent = dataf.get("NamePet");
+
+     NewCellRef = TableRowRef.insertCell(3);
+     NewCellRef.textContent = dataf.get("size");
+
+     NewCellRef = TableRowRef.insertCell(4);
+     NewCellRef.textContent = dataf.get("direccion");
+
+     NewCellRef = TableRowRef.insertCell(5);
+     NewCellRef.textContent = dataf.get("neighborhood");
+
+
+}
+
+    
