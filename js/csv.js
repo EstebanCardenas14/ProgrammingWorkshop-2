97e873ee5041172804d91973eaa65461ecb941b0
AndresGB <agalvisb@unbosque.edu.co>
2021-03-19 15:54:01 -0500

    Add to table is working, (no static data), im god you know?

--- js/csv.js
+++ js/csv.js
@@ -1,35 +1,55 @@
-var data;
-d3.dsv(";", "./data/pets-citizens.csv", function(d) {
-        return [
-            d.microchip,
-            d.species,
-            d.sex,
-            d.size,
-            d.potentDangerous,
-            d.neighborhood,
-        ];
-    }).then(function(data) {
-        $(document).ready(function() {
+let globaldata;
+d3.dsv(";", "./data/pets-citizens.csv", function (d) {
+    return [
+        d.microchip,
+        d.species,
+        d.sex,
+        d.size,
+        d.potentDangerous,
+        d.neighborhood,
+    ];
+}).then(function (data) {
+    $(document).ready(function () {
 
-            $('#table1').DataTable({
-                data: data,
-                columns: [
-                    { title: "microship" },
-                    { title: "species" },
-                    { title: "sex" },
-                    { title: "size" },
-                    { title: "pDangerous" },
-                    { title: "neighborhood" },
-                    { "race": "Null" },
-                    { "owner": "Null" },
-                    { "address": "Null" },
-                    { "defaultContent": `<button class = "btn btn-primary" data-bs-toggle="modal" data-bs-target="#editar">Actualizar</button>` },
-                ]
-            });
+        var t = $('#table1').DataTable({
 
+            data: data,
+            columns: [
+                { title: "microship" },
+                { title: "species" },
+                { title: "sex" },
+                { title: "size" },
+                { title: "pDangerous" },
+                { title: "neighborhood" },
+                { title: "race" },
+                { title: "owner" },
+                { title: "address" },
+                { "defaultContent": `<button class = "btn btn-primary" data-bs-toggle="modal" data-bs-target="#editar">Actualizar</button>` },
+            ]
         });
-    })
-    .catch(function(error) {
+        $('#buttonsave').on('click', function () {
+            console.log("asf")
+            t.row.add([
+                microship,
+                species,
+                sex,
+                size,
+                potentDangerous,
+                neighborhood,
+                race,
+                owner,
+                adress,
+                { "defaultContent": `<button class = "btn btn-primary" data-bs-toggle="modal" data-bs-target="#editar">Actualizar</button>` },
+
+            ]).draw(false);
+        });
+        // Automatically add a first row of data
+        $('#addRow').click();
+        globaldata = data;
+
+    });
+})
+    .catch(function (error) {
         // handle error
         console.log('error', error);
 
@@ -54,49 +74,7 @@ var LongitudFA = document.querySelector("#LongitudFA").value;
 // console.log(namePet, namePerson, race, direccion, latitude, longitude, species, neighborhood,size,image);
 var form = document.getElementById("formAdd");
 
-buttonSave = document.getElementById("buttonsave");
-buttonSave.addEventListener("click", () => {
 
 
-    var dataf = new FormData(form);
-    insertRowinTable(dataf)
-    console.log("hola");
 
-})
 
-function insertRowinTable(dataf) {
-    var TableRef = document.getElementById("table1");
-    var TableRowRef = TableRef.insertRow(1);
-
-    var NewCellRef = TableRowRef.insertCell(0);
-    NewCellRef.textContent = microship
-    NewCellRef = TableRowRef.insertCell(1);
-    NewCellRef.textContent = species;
-
-    NewCellRef = TableRowRef.insertCell(2);
-    NewCellRef.textContent = sex;
-
-    NewCellRef = TableRowRef.insertCell(3);
-    NewCellRef.textContent = size
-
-    NewCellRef = TableRowRef.insertCell(4);
-    NewCellRef.textContent = potentDangerous
-
-    NewCellRef = TableRowRef.insertCell(5);
-    NewCellRef.textContent = neighborhood
-
-    NewCellRef = TableRowRef.insertCell(6);
-    NewCellRef.textContent = ""
-
-    NewCellRef = TableRowRef.insertCell(7);
-    NewCellRef.textContent = race
-
-    NewCellRef = TableRowRef.insertCell(8);
-    NewCellRef.textContent = owner
-
-    NewCellRef = TableRowRef.insertCell(9);
-    NewCellRef.textContent = adress
-    data = data + [nameOwner, species, namePet, size, direccion, neighborhood]
-    console.log(data);
-
-}
