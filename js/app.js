b00dd5569067aef2749dab52c479831ef1d8d4f3
Paula Andrea Anaya Ramirez <panaya@unbosque.edu.co>
2021-03-20 09:25:17 -0500

    brushing some details in interfaz

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ const card = document.querySelector(".card-animals");
 var output = document.getElementById("map");
 var edit = document.getElementById("Button-edit0");
 var edit1 = document.getElementById("Button-edit1");
-var edit2= document.getElementById("Button-edit2");
+var edit2 = document.getElementById("Button-edit2");
 var BtnAddedit = document.getElementById("add-edit");
 var txtlatitude = document.getElementById("latitude");
 var txtlongitude = document.getElementById("longitude");
@@ -16,8 +16,6 @@ var btnx = document.getElementById("btnx")
 addAnimal.addEventListener("click", () => {
 
     formAddAnimal.style.display = "block";
-    BtnAddedit.innerText =("Añadir mascota");
-
 });
 
 btnCloseForm.addEventListener("click", () => {
@@ -27,55 +25,56 @@ btnCloseForm.addEventListener("click", () => {
 });
 
 function findMe() {
-  function localization(posicion) {
+    function localization(posicion) {
 
-    var latitude = posicion.coords.latitude;
-    var longitude = posicion.coords.longitude;
-    //Show the map
-    let map = L.map('map').setView([latitude, longitude], 15);
-    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-      maxZoom: 18
-    }).addTo(map);
-    //Add zoom 
-    L.control.scale().addTo(map);
-    //Add a marker
-    var x = new L.marker([latitude, longitude], { draggable: true }).addTo(map);
-    x.addEventListener("click", () => { 
-      txtlatitude.value = x.getLatLng().lat;
-      txtlongitude.value = x.getLatLng().lng;
-    
-      card.style.display = 'block';
-    });
-  }
-  function error() {
-    output.innerHTML = "<p>No se pudo obtener tu ubicación</p>";
-  }
-  navigator.geolocation.getCurrentPosition(localization, error);
+        var latitude = posicion.coords.latitude;
+        var longitude = posicion.coords.longitude;
+        //Show the map
+        let map = L.map('map').setView([latitude, longitude], 15);
+        L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+            maxZoom: 18
+        }).addTo(map);
+        //Add zoom 
+        L.control.scale().addTo(map);
+        //Add a marker
+        var x = new L.marker([latitude, longitude], { draggable: true }).addTo(map);
+        x.addEventListener("click", () => {
+            txtlatitude.value = x.getLatLng().lat;
+            txtlongitude.value = x.getLatLng().lng;
+
+            card.style.display = 'block';
+        });
+    }
+
+    function error() {
+        output.innerHTML = "<p>No se pudo obtener tu ubicación</p>";
+    }
+    navigator.geolocation.getCurrentPosition(localization, error);
 }
 imgmap.addEventListener("click", () => {
-  output.style.display = "block"
-  btnx.style.display = "block"
-  findMe()
+    output.style.display = "block"
+    btnx.style.display = "block"
+    findMe()
 });
 buttonmap.addEventListener("click", () => {
-  output.style.display = "block"
-  btnx.style.display = "block"
-  findMe()
+    output.style.display = "block"
+    btnx.style.display = "block"
+    findMe()
 });
-btnx.addEventListener( "click", ()=>{
-  output.style.display = "none";
-  btnx.style.display = "none"
+btnx.addEventListener("click", () => {
+    output.style.display = "none";
+    btnx.style.display = "none"
 });
 edit.addEventListener("click", () => {
     formAddAnimal.style.display = "block";
-    BtnAddedit.innerText =("Editar mascota");
+    BtnAddedit.innerText = ("Editar mascota");
 });
 edit1.addEventListener("click", () => {
-  formAddAnimal.style.display = "block";
-  BtnAddedit.innerText =("Editar mascota");
+    formAddAnimal.style.display = "block";
+    BtnAddedit.innerText = ("Editar mascota");
 });
 edit2.addEventListener("click", () => {
-  formAddAnimal.style.display = "block";
-  BtnAddedit.innerText =("Editar mascota");
+    formAddAnimal.style.display = "block";
+    BtnAddedit.innerText = ("Editar mascota");
 });
--- js/csv.js
+++ js/csv.js
@@ -2,73 +2,78 @@ let globaldata;
 var id = 0;
 var saveChanges = document.querySelector("#btnsavechanges");
 
-d3.dsv(";", "./data/pets-citizens.csv", function (d) {
-    return [
-        d.microchip,
-        d.species,
-        d.sex,
-        d.size,
-        d.potentDangerous,
-        d.neighborhood,
-    ];
-}).then(function (data) {
-    $(document).ready(function () {
+d3.dsv(";", "./data/pets-citizens.csv", function(d) {
+        return [
+            d.microchip,
+            d.species,
+            d.sex,
+            d.size,
+            d.potentDangerous,
+            d.neighborhood,
+        ];
+    }).then(function(data) {
+        $(document).ready(function() {
 
-        var t = $('#table1').DataTable({
-            data: data,
-            columns: [
-                { title: "microship" },
-                { title: "species" },
-                { title: "sex" },
-                { title: "size" },
-                { title: "pDangerous" },
-                { title: "neighborhood" },
-                { title: "race" },
-                { title: "owner" },
-                { title: "address" },
-                { "defaultContent": `<button class = "btn btn-primary" data-bs-toggle="modal" data-bs-target="#editar">Actualizar</button>` },
-            ],           
-             select: true
-        });
-        $('#table1 tbody').on( 'click', 'tr', function () {
-            if ( $(this).hasClass('selected') ) {
-                $(this).removeClass('selected');
-            }
-            else {
-                t.$('tr.selected').removeClass('selected');
-                $(this).addClass('selected');
-            }
-        } );
-        $('#btnsavechanges').on( 'click',function () {
-            if ( $(this).hasClass('selected') ) {
-            t.row('.selected').remove().draw( false );
-            }
-        } );
-     
-        $('#buttonsave').on('click', function () {
-            console.log("asf")
-            t.row.add([
-                microship,
-                species,
-                sex,
-                size,
-                potentDangerous,
-                neighborhood,
-                race,
-                owner,
-                adress,
-                { "defaultContent": `<button class = "btn btn-primary" data-bs-toggle="modal" data-bs-target="#editar">Actualizar</button>` },
-            ]).draw(false);
-        });
-        $('editar222').on('click', '.table-remove', function () { $(this).parents('tr').detach(); });
-        
-        // Automatically add a first row of data
-        $('#addRow').click();
-        globaldata = data;
+            var t = $('#table1').DataTable({
+                data: data,
+                columns: [
+                    { title: "microship" },
+                    { title: "species" },
+                    { title: "sex" },
+                    { title: "size" },
+                    { title: "pDangerous" },
+                    { title: "neighborhood" },
+                    { title: "race" },
+                    { title: "owner" },
+                    { title: "address" },
+                    { "defaultContent": `<button class = "btn btn-primary" data-bs-toggle="modal" data-bs-target="#editar">Actualizar</button>` },
+                ],
+                select: true
+            });
+            $('#table1 tbody').on('click', 'tr', function() {
+                if ($(this).hasClass('selected')) {
+                    $(this).removeClass('selected');
+                } else {
+                    t.$('tr.selected').removeClass('selected');
+                    $(this).addClass('selected');
+                }
+            });
+            $('#btnsavechanges').on('click', function() {
+                if ($(this).hasClass('selected')) {
+                    t.row('.selected').remove().draw(false);
+                }
+            });
 
-    });
-})
-    .catch(function (error) {
+            try {
+
+                $('#buttonsave').on('click', function() {
+                    console.log("asf")
+                    t.row.add([
+                        microship,
+                        species,
+                        sex,
+                        size,
+                        potentDangerous,
+                        neighborhood,
+                        race,
+                        owner,
+                        adress,
+                        { "defaultContent": `<button class = "btn btn-primary" data-bs-toggle="modal" data-bs-target="#editar">Actualizar</button>` },
+                    ]).draw(false);
+                });
+
+            } catch (Exception) {
+
+            }
+            $('editar222').on('click', '.table-remove', function() { $(this).parents('tr').detach(); });
+
+            // Automatically add a first row of data
+            $('#addRow').click();
+            globaldata = data;
+
+        });
+    })
+    .catch(function(error) {
         // handle error
         console.log('error', error);
 
@@ -92,8 +97,3 @@ var LongitudFA = document.querySelector("#LongitudFA").value;
 
 // console.log(namePet, namePerson, race, direccion, latitude, longitude, species, neighborhood,size,image);
 var form = document.getElementById("formAdd");
-
-
-
-
-
