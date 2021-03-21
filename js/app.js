4982c19bd836d879c3e487281424851e1542807f
AndresGB <agalvisb@unbosque.edu.co>
2021-03-20 23:10:53 -0500

    Everything is working except bono 1 and that's it

--- js/app.js
+++ js/app.js
@@ -3,14 +3,18 @@ const formAddAnimal = document.querySelector("#formAdd");
 const btnCloseForm = document.querySelector("#closeFormAdd");
 var imgmap = document.getElementById("img-map");
 var buttonmap = document.getElementById("buttonmap");
+var buttonmap2 = document.getElementById("buttonmap2");
 const card = document.querySelector(".card-animals");
 var output = document.getElementById("map");
+var output2 = document.getElementById("mapa2");
+
 var BtnAddedit = document.getElementById("add-edit");
 var txtlatitude = document.getElementById("latitude");
 var txtlongitude = document.getElementById("longitude");
-var btnx = document.getElementById("btnx")
+let btnx = document.querySelector("#btnx2")
 
-
+var arrlat = [];
+var arrlon = [];
 
 function findMe() {
   function localization(posicion) {
@@ -26,19 +30,46 @@ function findMe() {
     //Add zoom 
     L.control.scale().addTo(map);
     //Add a marker
-    var x = new L.marker([latitude, longitude], { draggable: true }).addTo(map);
-    x.addEventListener("click", () => { 
-      txtlatitude.value = x.getLatLng().lat;
-      txtlongitude.value = x.getLatLng().lng;
-    
-      card.style.display = 'block';
-    });
+    try {
+      for (let i = 0; i < arrlat1.length; i++) {
+        var x = new L.marker([arrlat[i], arrlon[i]], { draggable: true }).addTo(map);
+      }
+    } catch (error) {
+      
+    }
+   
   }
   function error() {
     output.innerHTML = "<p>No se pudo obtener tu ubicación</p>";
   }
   navigator.geolocation.getCurrentPosition(localization, error);
 }
+function findMe2() {
+  function localization2(posicion2) {
+
+    let latitude2 = posicion2.coords.latitude;
+    let longitude2 = posicion2.coords.longitude;
+    //Show the map
+    let map2 = L.map('mapa2').setView([latitude2, longitude2], 15);
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+      maxZoom: 18
+    }).addTo(map2);
+    //Add zoom 
+    L.control.scale().addTo(map2);
+    //Add a marker
+    let x2 = new L.marker([latitude2, longitude2], { draggable: true }).addTo(map2);
+    x.addEventListener("click", () => { 
+      arrlat1.push( x2.getLatLng().lat)
+      arrlon2.push(x2.getLatLng().lng)
+    });
+  }
+  function error2() {
+    output2.innerHTML = "<p>No se pudo obtener tu ubicación</p>";
+  }
+  navigator.geolocation.getCurrentPosition(localization2, error2);
+}
+
 
 
 addAnimal.addEventListener("click", () => {
@@ -49,11 +80,19 @@ addAnimal.addEventListener("click", () => {
 });
 
 btnCloseForm.addEventListener("click", () => formAddAnimal.style.display = "none");
-
+btnx.addEventListener( "click", ()=>{
+  console.log("safasf")
+  output.style.display = "none";
+  btnx.style.display = "none"
+});
 imgmap.addEventListener("click", () => {
   output.style.display = "block"
   btnx.style.display = "block"
   findMe()
+});
+buttonmap2.addEventListener("click", () => {
+  
+  findMe2();
 });
 
 buttonmap.addEventListener("click", () => {
@@ -61,7 +100,4 @@ buttonmap.addEventListener("click", () => {
   btnx.style.display = "block"
   findMe()
 });
-btnx.addEventListener( "click", ()=>{
-  output.style.display = "none";
-  btnx.style.display = "none"
-});
+
